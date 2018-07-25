9f3e2ca470555c41c64c7152667f1c59c6fab664
FMCalisto <FMCalisto@users.noreply.github.com>
2018-07-25 15:53:59 +0100

    [UPDATE] File Systems Structure

--- src/myAverageTime.py
+++ src/myAverageTime.py
@@ -1,4 +1,5 @@
 import os, json
+import sys, os.path
 from pprint import pprint
 
 timeCounter = 0;
@@ -6,7 +7,11 @@ i = 0;
 filesNum = 12;
 totalTime = 0;
 
-path_to_json = '../../dataset-samples/counters/'
+joinPath = os.path.join(os.path.dirname(__file__), '..', '..')
+pathAbsPath = os.path.abspath(joinPath)
+
+path_to_json = os.path.join(joinPath, 'dataset-samples', 'counters', '')
+
 json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
 
 while i < filesNum:
@@ -16,5 +21,5 @@ while i < filesNum:
   totalTime = totalTime + data["rawData"]["time"]
   averageTime = totalTime / filesNum
   averageTime = averageTime + data["rawData"]["time"]
-  
+
 pprint(averageTime)
