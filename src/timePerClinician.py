836a9647366cf4e2f5eeea96811124188e569284
FMCalisto <FMCalisto@users.noreply.github.com>
2018-07-26 20:16:37 +0100

    [FIX] Code

--- src/timePerClinician.py
+++ src/timePerClinician.py
@@ -27,32 +27,31 @@ import sys, os.path
 from pprint import pprint
 
 # Index variables of "for" loop.
-i = 0;
-j = 1;
+i = 0
+j = 1
 
 # Insert manually the total number of files.
-filesNum = 12;
+filesNum = 12
 
 # Variable to recursively accumulate the total time number.
-totalTime = 0;
+totalTime = 0
 
 # The current folder path.
-basePath = os.path.dirname(__file__);
+basePath = os.path.dirname(__file__)
 
 # The path to the repository "root" folder.
-joinPath = os.path.join(basePath, '..', '..');
-pathAbsPath = os.path.abspath(joinPath);
+joinPath = os.path.join(basePath, '..', '..')
+pathAbsPath = os.path.abspath(joinPath)
 
 # The path for the "dataset-samples" repository and "counters" folder.
-path_to_json = os.path.join(joinPath, 'dataset-samples', 'counters', '');
-
-# JSON struture auxiliars iterators.
-jsonListDir = os.listdir(path_to_json);
-jsonFormat = pos_json.endswith('.json');
+path_to_json = os.path.join(joinPath, 'dataset-samples', 'counters', '')
 
 # Get all the JSON files inside the "counters" folder of
 # "dataset-samples" repository.
-json_files = [pos_json for pos_json in jsonListDir if jsonFormat]
+json_files = [
+  pos_json for pos_json in os.listdir(path_to_json)
+  if pos_json.endswith('.json')
+]
 
 # Per each file "i". For instance, "i = 0" the first element
 # will be the "1948.json" file.
@@ -60,21 +59,21 @@ for i in range(filesNum):
   # The following will iteratively read each JSON file. It starts
   # on the first ("i = 0") file of the folder.
   with open(path_to_json + json_files[i]) as f:
-    data = json.load(f);
+    data = json.load(f)
   # Per each file "j". For instance, "j = 1" the second element
   # will be the "1949.json" file.
   for j in range(filesNum):
     # The following will also iteratively read each JSON file. It starts
     # on the second ("j = 1") file of the folder.
     with open(path_to_json + json_files[j]) as fCompare:
-      dataCompare = json.load(fCompare);
+      dataCompare = json.load(fCompare)
     # Comparing both "i = 0" with "j = 1" and see if the "clinician"
     # is the same between both files. If so, it sums the older time
     # with the new one.
     if data['rawData']['clinician'] == dataCompare['rawData']['clinician']:
-      totalTime = totalTime + dataCompare['rawData']['time'];
-    j = j + 1;
-  clinicianName = data['rawData']['clinician'];
-  print("The Total Time of " + clinicianName + " is ", totalTime);
-  totalTime = 0;
-  i = i + 1;
+      totalTime = totalTime + dataCompare['rawData']['time']
+    j = j + 1
+  clinicianName = data['rawData']['clinician']
+  print("The Total Time of " + clinicianName + " is ", totalTime)
+  totalTime = 0
+  i = i + 1
