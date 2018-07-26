5dabb728a981e1e827d76eb6edfe27374282ace0
NinjaDragon98 <38191318+NinjaDragon98@users.noreply.github.com>
2018-07-26 21:30:09 +0100

    Add files via upload

---
+++ src/AnnotationDoctorPerSeriesDescription.py
@@ -0,0 +1,36 @@
+import os, json
+import sys, os.path
+from pprint import pprint
+
+i = 0;
+j = 1;
+
+filesNum = 12;
+totalAnnotation = 0;
+
+basePath = os.path.dirname(__file__);
+
+joinPath = os.path.join(os.path.dirname(__file__), '..', '..')
+pathAbsPath = os.path.abspath(joinPath)
+
+path_to_json = os.path.join(joinPath, 'dataset-samples', 'counters', '')
+
+jsonListDir = os.listdir(path_to_json);
+jsonFormat = pos_json.endswith('.json');
+
+json_files = [pos_json for pos_json in jsonListDir if jsonFormat]
+
+for i in range(filesNum):
+  with open(path_to_json + json_files[i]) as f:
+    data = json.load(f);
+  for j in range(filesNum):
+    with open(path_to_json + json_files[j]) as fCompare:
+      dataCompare = json.load(fCompare);
+    if data['rawData']['clinician'] == dataCompare['rawData']['clinician']:
+      totalAnnotation = totalAnnotation + dataCompare["rawData"]["stacks"][0]["seriesDescription"];
+    j = j + 1;
+  print("The Total Annotation of " + data["rawData"]["stacks"][0]["seriesDescription"] + " is ", totalAnnotation);
+  totalAnnotation = 0;
+  i = i + 1;
+  
+  
