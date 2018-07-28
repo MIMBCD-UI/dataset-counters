ffcac3acd7e481fb17473d20e18fe82280fe3b6f
NinjaDragon98 <duartefigueiroa_98@hotmail.com>
2018-07-28 16:03:14 +0100

    new files

---
+++ src/myAverageTime.py
@@ -0,0 +1,34 @@
+import os, json
+import sys, os.path
+from pprint import pprint
+
+i = 0;
+j = 1;
+
+filesNum = 12;
+totalTime = 0;
+
+basePath = os.path.dirname(__file__);
+
+joinPath = os.path.join(os.path.dirname(__file__), '..', '..')
+pathAbsPath = os.path.abspath(joinPath)
+
+path_to_json = os.path.join(joinPath, 'dataset-samples', 'counters', '')
+
+jsonListDir = os.listdir(path_to_json);
+jsonFormat = pos_json.endswith('.json');
+
+json_files = [pos_json for pos_json in jsonListDir if jsonFormat]
+
+for i in range(filesNum):
+  with open(path_to_json + json_files[i]) as f:
+    data = json.load(f);
+  for j in range(filesNum):
+    with open(path_to_json + json_files[j]) as fCompare:
+      dataCompare = json.load(fCompare);
+    if data['rawData']['clinician'] == dataCompare['rawData']['clinician']:
+      totalTime = totalTime + dataCompare['rawData']['time'];
+    j = j + 1;
+  print("The Total Time of " + data['rawData']['clinician'] + " is ", totalTime);
+  totalTime = 0;
+  i = i + 1;
